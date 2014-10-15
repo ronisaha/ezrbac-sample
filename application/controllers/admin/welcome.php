182e786a040590fd93ab31f89c8d882e48d95bc7
Roni Saha <roni.cse@gmail.com>
2014-10-15 08:23:45 +0600

    create api example added

--- application/controllers/admin/welcome.php
+++ application/controllers/admin/welcome.php
@@ -6,6 +6,7 @@ class Welcome extends CI_Controller {
 	public function access_map(){
         return array(
             'index'=>'view',
+            'create_user'=>'view',
             'greeting'=>'edit'
         );
     }
@@ -14,6 +15,19 @@ class Welcome extends CI_Controller {
 	{
         $this->load->view('welcome_message');
 	}
+
+    public function create_user()
+    {
+       $x =  $this->ezrbac->createUser(array(
+            'user_role' => 'Admin',
+            'meta' => array('first_name'=>'First', 'last_name'=>'Last'),
+            'email' => 'email@domain.com',
+            'password' => 'secret',
+            'status' => '1'
+        ));
+
+        echo "User created with user id : " . $x;
+    }
 
     public function greeting()
     {
